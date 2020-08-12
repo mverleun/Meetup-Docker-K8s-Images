e3e1f4ba451eb09da01bb154ac268eb27bb19779
Marco Verleun <marco@verleun.org>
2020-08-12 19:21:01 +0200

    Demo-1

---
+++ Demo-1/step-1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo Starting MariaDB
+
+docker-compose up db
---
+++ Demo-1/step-3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo Starting Nextcloud 13
+
+echo Open a browser for http://127.0.0.1:8013
+
+docker-compose up nc-13
